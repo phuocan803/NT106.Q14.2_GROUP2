dea8d5ae61db4b5d7e6f123d7c2c905e5716457a
chillan <annguyen020106@gmail.com>
2025-12-08 17:35:35 +0700

    update logic Lab04_Bai04, Lab04_Bai05+06

---
+++ Bai04/Bai04-QuanLyPhongVe.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Bai04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.btnBookTicket = new System.Windows.Forms.Button();
+            this.lblSelectedMovie = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCrawl.FlatAppearance.BorderSize = 0;
+            this.btnCrawl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrawl.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrawl.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnCrawl.Location = new System.Drawing.Point(252, 524);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(117, 34);
+            this.btnCrawl.TabIndex = 0;
+            this.btnCrawl.Text = "Tải Phim";
+            this.btnCrawl.UseVisualStyleBackColor = false;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(252, 574);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 22);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(384, 50);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(338, 508);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tên Khách hàng";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCustomerName.Location = new System.Drawing.Point(176, 50);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(193, 22);
+            this.txtCustomerName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(23, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số vé";
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(176, 104);
+            this.numQuantity.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numQuantity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(120, 22);
+            this.numQuantity.TabIndex = 6;
+            this.numQuantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnBookTicket
+            // 
+            this.btnBookTicket.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnBookTicket.FlatAppearance.BorderSize = 0;
+            this.btnBookTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBookTicket.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBookTicket.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnBookTicket.Location = new System.Drawing.Point(176, 166);
+            this.btnBookTicket.Name = "btnBookTicket";
+            this.btnBookTicket.Size = new System.Drawing.Size(83, 40);
+            this.btnBookTicket.TabIndex = 7;
+            this.btnBookTicket.Text = "Đặt vé";
+            this.btnBookTicket.UseVisualStyleBackColor = false;
+            this.btnBookTicket.Click += new System.EventHandler(this.btnBookTicket_Click);
+            // 
+            // lblSelectedMovie
+            // 
+            this.lblSelectedMovie.AutoSize = true;
+            this.lblSelectedMovie.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectedMovie.ForeColor = System.Drawing.Color.Black;
+            this.lblSelectedMovie.Location = new System.Drawing.Point(173, 146);
+            this.lblSelectedMovie.Name = "lblSelectedMovie";
+            this.lblSelectedMovie.Size = new System.Drawing.Size(103, 17);
+            this.lblSelectedMovie.TabIndex = 8;
+            this.lblSelectedMovie.Text = "Chưa chọn phim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(391, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Phim Đang Chiếu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 618);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSelectedMovie);
+            this.Controls.Add(this.btnBookTicket);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCrawl);
+            this.Name = "Form1";
+            this.Text = "QuanLyPhongVe";
+            this.Load += new System.EventHandler(this.btnBookTicket_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrawl;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Button btnBookTicket;
+        private System.Windows.Forms.Label lblSelectedMovie;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Bai05+06/.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Bai05_06
+{
+    partial class Bai05_06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtUserInfo_FullName = new System.Windows.Forms.TextBox();
+            this.txtUserInfo_Email = new System.Windows.Forms.TextBox();
+            this.txtUserInfo_Username = new System.Windows.Forms.TextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Đăng Nhập";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(143, 142);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(104, 38);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(143, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 25);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(143, 41);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 25);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtUserInfo_FullName);
+            this.groupBox2.Controls.Add(this.txtUserInfo_Email);
+            this.groupBox2.Controls.Add(this.txtUserInfo_Username);
+            this.groupBox2.Controls.Add(this.rtbResult);
+            this.groupBox2.Controls.Add(this.btnGetInfo);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(407, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 461);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin người dùng";
+            // 
+            // txtUserInfo_FullName
+            // 
+            this.txtUserInfo_FullName.Location = new System.Drawing.Point(161, 116);
+            this.txtUserInfo_FullName.Name = "txtUserInfo_FullName";
+            this.txtUserInfo_FullName.Size = new System.Drawing.Size(150, 25);
+            this.txtUserInfo_FullName.TabIndex = 9;
+            // 
+            // txtUserInfo_Email
+            // 
+            this.txtUserInfo_Email.Location = new System.Drawing.Point(161, 79);
+            this.txtUserInfo_Email.Name = "txtUserInfo_Email";
+            this.txtUserInfo_Email.Size = new System.Drawing.Size(150, 25);
+            this.txtUserInfo_Email.TabIndex = 8;
+            // 
+            // txtUserInfo_Username
+            // 
+            this.txtUserInfo_Username.Location = new System.Drawing.Point(161, 42);
+            this.txtUserInfo_Username.Name = "txtUserInfo_Username";
+            this.txtUserInfo_Username.Size = new System.Drawing.Size(150, 25);
+            this.txtUserInfo_Username.TabIndex = 7;
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbResult.Location = new System.Drawing.Point(3, 217);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(344, 241);
+            this.rtbResult.TabIndex = 6;
+            this.rtbResult.Text = "";
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetInfo.Location = new System.Drawing.Point(161, 162);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(150, 41);
+            this.btnGetInfo.TabIndex = 5;
+            this.btnGetInfo.Text = "Nhận thông tin";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Họ và Tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "UserName";
+            // 
+            // Bai05_06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Bai05_06";
+            this.Text = "Bai05_06";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserInfo_FullName;
+        private System.Windows.Forms.TextBox txtUserInfo_Email;
+        private System.Windows.Forms.TextBox txtUserInfo_Username;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Button btnGetInfo;
+    }
+}
+
